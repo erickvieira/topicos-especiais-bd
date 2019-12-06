d6fc94e58015699c9b2f59e905bee65ccf97a88a
Erick Vieira <erick-vieira-s@outlook.com>
2019-12-06 16:15:24 -0200

    ATUALIZANDO O RASCUNHO

---
+++ rascunho.sql
@@ -0,0 +1,28 @@
+-- log das mudanças salariais dos funcionarios
+CREATE TABLE log_salario (
+    codigo NUMBER(7, 2) PRIMARY KEY,
+    salario_anterior NUMBER(7, 2),
+    salario_atual NUMBER(7, 2),
+    data_alteracao DATE,
+    usuario VARCHAR2(40)
+);
+
+-- gatilho da mudança salarial
+CREATE OR REPLACE TRIGGER trg_salario_aud
+AFTER UPDATE ON EMP
+REFERENCING NEW AS NEW OLD AS OLD
+FOR EACH ROW
+BEGIN
+    INSERT INTO log_salario(codigo, salario_anterior, salario_atual, data_alteracao, usuario)
+    VALUES (:NEW.empno, :OLD.sal, :NEW.sal, SYSDATE, USER);
+END;
+
+-- mudando o salário do funcionário nº 5000
+UPDATE EMP SET SAL = SAL * 1.1 WHERE empno = 5000;
+
+-- confirmando se a trigger rodou
+SELECT * FROM log_salario;
+-- RESULTADO:
+--     CODIGO SALARIO_ANTERIOR SALARIO_ATUAL DATA_ALT USUARIO
+-- ---------- ---------------- ------------- -------- -------------
+--       5000             1100          1210 06/12/19 USER1        
